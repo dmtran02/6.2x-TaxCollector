405c083fc90e4dc368230c15f9b7402aa7fff46f
Duc Tran <dmtran02@gmail.com>
2019-02-04 19:17:14 -0500

    updated

--- TaxCollector/Form1.Designer.cs
+++ TaxCollector/Form1.Designer.cs
@@ -34,21 +34,28 @@
             this.txtTaxableIncome = new System.Windows.Forms.TextBox();
             this.txtIncomeTaxOwed = new System.Windows.Forms.TextBox();
             this.btnExit = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnNameOK = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnCalculate
             // 
-            this.btnCalculate.Location = new System.Drawing.Point(46, 103);
+            this.btnCalculate.Enabled = false;
+            this.btnCalculate.Location = new System.Drawing.Point(46, 138);
             this.btnCalculate.Name = "btnCalculate";
             this.btnCalculate.Size = new System.Drawing.Size(75, 23);
             this.btnCalculate.TabIndex = 0;
             this.btnCalculate.Text = "Calculate";
             this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            this.btnCalculate.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EnterPress);
             // 
             // lblTaxableIncome
             // 
             this.lblTaxableIncome.AutoSize = true;
-            this.lblTaxableIncome.Location = new System.Drawing.Point(13, 33);
+            this.lblTaxableIncome.Enabled = false;
+            this.lblTaxableIncome.Location = new System.Drawing.Point(13, 68);
             this.lblTaxableIncome.Name = "lblTaxableIncome";
             this.lblTaxableIncome.Size = new System.Drawing.Size(86, 13);
             this.lblTaxableIncome.TabIndex = 1;
@@ -57,7 +64,8 @@
             // lblIncomeTaxOwed
             // 
             this.lblIncomeTaxOwed.AutoSize = true;
-            this.lblIncomeTaxOwed.Location = new System.Drawing.Point(12, 66);
+            this.lblIncomeTaxOwed.Enabled = false;
+            this.lblIncomeTaxOwed.Location = new System.Drawing.Point(12, 101);
             this.lblIncomeTaxOwed.Name = "lblIncomeTaxOwed";
             this.lblIncomeTaxOwed.Size = new System.Drawing.Size(97, 13);
             this.lblIncomeTaxOwed.TabIndex = 2;
@@ -65,32 +73,65 @@
             // 
             // txtTaxableIncome
             // 
-            this.txtTaxableIncome.Location = new System.Drawing.Point(139, 33);
+            this.txtTaxableIncome.Enabled = false;
+            this.txtTaxableIncome.Location = new System.Drawing.Point(139, 68);
             this.txtTaxableIncome.Name = "txtTaxableIncome";
             this.txtTaxableIncome.Size = new System.Drawing.Size(100, 20);
             this.txtTaxableIncome.TabIndex = 3;
             // 
             // txtIncomeTaxOwed
             // 
-            this.txtIncomeTaxOwed.Location = new System.Drawing.Point(139, 63);
+            this.txtIncomeTaxOwed.Enabled = false;
+            this.txtIncomeTaxOwed.Location = new System.Drawing.Point(139, 98);
             this.txtIncomeTaxOwed.Name = "txtIncomeTaxOwed";
             this.txtIncomeTaxOwed.Size = new System.Drawing.Size(100, 20);
             this.txtIncomeTaxOwed.TabIndex = 4;
             // 
             // btnExit
             // 
-            this.btnExit.Location = new System.Drawing.Point(162, 103);
+            this.btnExit.Location = new System.Drawing.Point(162, 138);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 5;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            this.btnExit.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EscPress);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(13, 13);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(64, 13);
+            this.lblName.TabIndex = 6;
+            this.lblName.Text = "Enter name:\r\n";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(95, 10);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 7;
+            // 
+            // btnNameOK
+            // 
+            this.btnNameOK.Location = new System.Drawing.Point(208, 8);
+            this.btnNameOK.Name = "btnNameOK";
+            this.btnNameOK.Size = new System.Drawing.Size(75, 23);
+            this.btnNameOK.TabIndex = 8;
+            this.btnNameOK.Text = "OK";
+            this.btnNameOK.UseVisualStyleBackColor = true;
+            this.btnNameOK.Click += new System.EventHandler(this.btnNameOK_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(295, 149);
+            this.ClientSize = new System.Drawing.Size(295, 178);
+            this.Controls.Add(this.btnNameOK);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.txtIncomeTaxOwed);
             this.Controls.Add(this.txtTaxableIncome);
@@ -98,7 +139,7 @@
             this.Controls.Add(this.lblTaxableIncome);
             this.Controls.Add(this.btnCalculate);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Income Tax Calculator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -112,6 +153,9 @@
         private System.Windows.Forms.TextBox txtTaxableIncome;
         private System.Windows.Forms.TextBox txtIncomeTaxOwed;
         private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnNameOK;
     }
 }
 
--- TaxCollector/Form1.cs
+++ TaxCollector/Form1.cs
@@ -15,6 +15,111 @@ namespace TaxCollector
         public Form1()
         {
             InitializeComponent();
+            txtName.Focus();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (ValidateTaxableIncome())
+            {
+                double income = double.Parse(txtTaxableIncome.Text);
+                txtIncomeTaxOwed.Text = TaxTableLookUp(income).ToString();
+                txtTaxableIncome.Focus();
+            }
+        }
+
+        private void btnNameOK_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtName.Text))
+            {
+                MessageBox.Show("Please enter a name to continue.", "No Name Found!");
+                txtName.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Welcome " + txtName.Text + "!", "Welcome!");
+                txtName.ReadOnly = true;
+                btnNameOK.Visible = false;
+                lblTaxableIncome.Enabled = true;
+                lblIncomeTaxOwed.Enabled = true;
+                txtTaxableIncome.Enabled = true;
+                btnCalculate.Enabled = true;
+                txtTaxableIncome.Focus();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void EnterPress(object sender, KeyPressEventArgs e)
+        {
+            btnCalculate_Click(sender, e);
+        }
+
+        private void EscPress(object sender, KeyPressEventArgs e)
+        {
+            btnExit_Click(sender, e);
+        }
+
+        public bool ValidateTaxableIncome()
+        {
+            bool result = false;
+            double income;
+            bool success = double.TryParse(txtTaxableIncome.Text, out income);
+            if (success)
+            {
+                Console.WriteLine(income + " is a valid value.");
+                result = true;
+                return result;
+            }
+            else
+            {
+                MessageBox.Show("The value you have entered is not a valid number.", "Incorrect Input!");
+                Console.WriteLine(income + " is not a valid value.");
+                txtTaxableIncome.Focus();
+            }
+
+            return result;
+        }
+
+        public double TaxTableLookUp(double taxableIncome)
+        {
+            double taxOwed = 0;
+            if(taxableIncome > 0 && taxableIncome <= 9225)
+            {
+                taxOwed = 0 + 0.10 * (taxableIncome - 0);
+            }
+            else if(taxableIncome > 9225 && taxableIncome <= 37450)
+            {
+                taxOwed = 922.50 + 0.15 * (taxableIncome - 9225);
+            }
+            else if(taxableIncome > 37450 && taxableIncome <= 90750)
+            {
+                taxOwed = 5156.25 + 0.25 * (taxableIncome - 37450);
+            }
+            else if(taxableIncome > 90750 && taxableIncome <= 189300)
+            {
+                taxOwed = 18481.25 + 0.28 * (taxableIncome - 90750);
+            }
+            else if(taxableIncome > 189300 && taxableIncome <= 411500)
+            {
+                taxOwed = 46075.25 + 0.33 * (taxableIncome - 189300);
+            }
+            else if(taxableIncome > 411500 && taxableIncome <= 413200)
+            {
+                taxOwed = 119401.25 + 0.35 * (taxableIncome - 411500);
+            }
+            else if(taxableIncome < 413200)
+            {
+                taxOwed = 119996.25 + 0.396 * (taxableIncome - 413200);
+            }
+            else
+            {
+                taxOwed = -1.0;
+            }
+            return taxOwed;
         }
     }
 }
