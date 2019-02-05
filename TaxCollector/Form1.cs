2eefad6e2f9bc0c7a278e3b8c2bb9d0221644da8
Duc Tran <dmtran02@gmail.com>
2019-02-04 21:43:19 -0500

    added comments and finalized project

--- TaxCollector/Form1.cs
+++ TaxCollector/Form1.cs
@@ -8,6 +8,13 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * Duc Tran
+ * Professor Frank Friedman
+ * CIS 3309 - Assignment 6.2x // TaxCollector
+ * Last Updated: February 4, 2019
+ */
+
 namespace TaxCollector
 {
     public partial class Form1 : Form
@@ -18,6 +25,11 @@ namespace TaxCollector
             txtName.Focus();
         }
 
+        /*
+         * Uses the ValidateTaxableIncome to confirm if user input is a valid number and calculate the 
+         * appropriate tax owed based on the Tax Table used in the TaxTableLookUp method.
+         */
+
         private void btnCalculate_Click(object sender, EventArgs e)
         {
             if (ValidateTaxableIncome())
@@ -27,6 +39,13 @@ namespace TaxCollector
                 txtTaxableIncome.Focus();
             }
         }
+
+        /*
+         * Checks if user's name is present in the text box.
+         *  -If present, program welcomes user and enables TaxIncome and IncomeTaxOwed labels and txtTaxableIncome,
+         *   btnNameOK becomes invisible, txtName is read-only and txtTaxableIncome is focused.
+         *  -Otherwise, a message box will ask user to input a name.
+         */
 
         private void btnNameOK_Click(object sender, EventArgs e)
         {
@@ -48,20 +67,38 @@ namespace TaxCollector
             }
         }
 
+        /*
+         * When the Exit button is clicked, the application will close.
+         */
+
         private void btnExit_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+        /*
+         * Event handler for the Calculate function when the 'Enter' key is pressed, the tax owed is calculated.
+         */
 
         private void EnterPress(object sender, KeyPressEventArgs e)
         {
             btnCalculate_Click(sender, e);
         }
 
+        /*
+         * Event handler for the Exit function when the 'Esc' key is pressed, the application closes.
+         */
+
         private void EscPress(object sender, KeyPressEventArgs e)
         {
             btnExit_Click(sender, e);
         }
+
+        /*
+         * Method that returns a boolean variable to determine whether the user-inputted income is a valid value.
+         *  -Returns true if input is a valid value
+         *  -Otherwise, returns false for an invalid input and displays a message box requesting the user to enter a valid number
+         */
 
         public bool ValidateTaxableIncome()
         {
@@ -83,6 +120,10 @@ namespace TaxCollector
 
             return result;
         }
+
+        /*
+         * Aids in the calculation of tax owed based on a table from the IRS provided and the user-inputted income.
+         */
 
         public double TaxTableLookUp(double taxableIncome)
         {
